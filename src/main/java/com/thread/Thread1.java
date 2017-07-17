c12d6d4973cab924eb2f385a36a42f3ebcaecac5
贾新轩 <xinxuanjia@caixin.com>
2017-07-17 10:22:35 +0800

    多线程练习

---
+++ src/main/java/com/thread/Thread1.java
@@ -0,0 +1,33 @@
+package com.thread;
+/**
+ * 实现多线程runable方法
+ * @author Administrator
+ *
+ */
+public class Thread1 {
+	
+	public static  Integer num=0;
+	public static void main(String[] args) {
+		
+		Thread the1=new Thread(new Runnable() {
+			public void run() {
+				while(num<=100){
+					System.out.println(add());
+				}
+			}
+		});
+		the1.start();
+		Thread the2=new Thread(new Runnable() {
+			public void run() {
+				while(num<=100){
+					System.out.println(add());
+				}
+			}
+		});
+		the2.start();
+	}
+	
+	private static int add(){
+		return num++;
+	}
+}
