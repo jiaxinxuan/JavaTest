4e3c0c3c05cc178fa3a38344d433e9461268e46c
贾新轩 <xinxuanjia@caixin.com>
2017-08-14 10:58:31 +0800

    jdk1.8接口测试

---
+++ src/main/java/com/test/interfac/MethodInterfaceTest.java
@@ -0,0 +1,14 @@
+package com.test.interfac;
+/**
+ * java接口，默认方法和静态方法的测试
+ * @author Administrator
+ *
+ */
+public class MethodInterfaceTest implements Test{
+	
+	public static void main(String[] args) {
+		MethodInterfaceTest methodInterfaceTest=new MethodInterfaceTest();
+				methodInterfaceTest.hehe();
+	}
+
+}
---
+++ src/main/java/com/test/interfac/Test.java
@@ -0,0 +1,10 @@
+package com.test.interfac;
+
+public interface Test {
+	default void hehe(){
+		System.out.println("这个是默认方法!");
+	}
+	static void hehee(){
+		System.out.println("这里是静太方法！");
+	}
+}
