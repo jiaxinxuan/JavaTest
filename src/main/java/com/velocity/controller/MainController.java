408d595bfdff526b0b5829313469317996740b02
jiaxinxuan <jiaxinxuan@lvmama.com>
2018-11-01 15:24:35 +0800

    可运行保证

--- src/main/java/com/velocity/controller/MainController.java
+++ src/main/java/com/velocity/controller/MainController.java
@@ -1,54 +1,52 @@
 package com.velocity.controller;
- 
-import java.io.*;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import javax.servlet.http.*;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.annotation.SystemControllerLog;
 import com.test.entity.User;
 import com.velocity.service.Department;
 import com.velocity.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.support.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Controller
 public class MainController {
  
    @Autowired
    private DepartmentService deptService;
- 
+
    @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
    @SystemControllerLog(description ="欢迎，热烈欢迎！")
    public String welcomePage(Model model) throws Exception {
        List<Department> list = deptService.listDepartment();
-       model.addAttribute("departments", list);   
-       return "views/index";
+       model.addAttribute("departments", list);
+       return "api/index";
    }
-   
+
    @RequestMapping(value = { "/", "/exit" }, method = RequestMethod.GET)
    @SystemControllerLog(description ="再见，欢迎下次再来！")
    public String exitPage(Model model) {
        return "layouts/layout";
    }
-   
+
    @RequestMapping("/login")
    public String login(HttpServletRequest request){
 	   User user=new User();
 	   user.setName("贾新轩");
 	   HttpSession session=request.getSession();
 	   session.setAttribute("user", user);
-	   return "redirect:/welcome";
+	   return "redirect:/test";
    }
 
     @RequestMapping("/test")
